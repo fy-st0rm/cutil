60903596ba3ae8f80e5a9117db7a15da207c05c8
fy-st0rm <slokpradhan2062@gmail.com>
2022-10-22 19:20:16 +0545

    Updated assertion

--- misc.h
+++ misc.h
@@ -2,7 +2,7 @@
 #define _ASSERT_H_
 
 #include <stdio.h>
-#define __assert(...) { printf(__VA_ARGS__); exit(1); }
+#define __assert(...) { printf("\033[31m[ASSERTION]: %s:%d:\033[0m ", __FILE__, __LINE__); printf(__VA_ARGS__); exit(1); }
 #define cutil_assert(x, ...) x ? 0 : (__assert(__VA_ARGS__))
 
 #endif
